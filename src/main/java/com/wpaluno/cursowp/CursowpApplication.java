3fce1bf8a2df382fae423c838176566e978301cb
LuidRocha <luidrocha@yahoo.com.br>
2020-09-22 20:23:20 -0300

    Projeto Criado

---
+++ src/main/java/com/wpaluno/cursowp/CursowpApplication.java
@@ -0,0 +1,13 @@
+package com.wpaluno.cursowp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursowpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursowpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wpaluno/cursowp/CursowpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wpaluno.cursowp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursowpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
