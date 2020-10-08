b4e8f1382c81348279eae76816e4c5dee5455fa9
LuidRocha <luidrocha@yahoo.com.br>
2020-10-07 21:09:22 -0300

    Criando operacao de instanciacao

--- src/main/java/com/wpaluno/cursowp/CursowpApplication.java
+++ src/main/java/com/wpaluno/cursowp/CursowpApplication.java
@@ -1,13 +1,36 @@
 package com.wpaluno.cursowp;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.wpaluno.cursowp.domain.Categoria;
+import com.wpaluno.cursowp.repository.CategoriaRepository;
+
 @SpringBootApplication
-public class CursowpApplication {
+// CommandLineRunner{ faz com que possamos executar um metodo auxiliar ao carregar a app.
+
+public class CursowpApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursowpApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Contabilidade");
+		Categoria cat3 = new Categoria(null, "Suporte");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
 	}
 
 }
